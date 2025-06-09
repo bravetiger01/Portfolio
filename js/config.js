bc4613dfecbcce398adb0fbddb530f088f7dea63
Nakul Desai <125589034+bravetiger01@users.noreply.github.com>
2025-06-09 19:05:26 +0530

    Remove config.js from version control

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const emailjsConfig = {
-    PUBLIC_KEY: 't2UaXKm7KVDPlHgmq', // Replace with your EmailJS Public Key
-    SERVICE_ID: 'service_gynorii', // Replace with your EmailJS Service ID
-    TEMPLATE_ID: 'template_4522q98' // Replace with your EmailJS Template ID
-};
